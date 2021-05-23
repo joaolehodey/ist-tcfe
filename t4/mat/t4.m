fe3770d443e11b538d9ab89dc325ad19e803db34
joaolehodey <joao.lehodey@gmail.com>
2021-05-23 23:09:56 +0100

    op theoretical and some changes

--- t4/mat/t4.m
+++ t4/mat/t4.m
@@ -27,16 +27,17 @@ end
 
 vector;
 
-VT = str2double(vector(1)) 
-BFN = str2double(vector(2)) 
-VAFN = str2double(vector(3)) 
-RE1 = str2double(vector(4))      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-RC1 = str2double(vector(5))      %  values from datagen
-RB1 = str2double(vector(6))      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-RB2 = str2double(vector(7)) 
-VBEON = str2double(vector(8)) 
-VCC = str2double(vector(9)) 
-RS = str2double(vector(10)) 
+VT = str2double(vector(1)) ;
+BFN = str2double(vector(2)) ;
+VAFN = str2double(vector(3)) ;
+RE1 = str2double(vector(4))  ;    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+RC1 = str2double(vector(5)) ;     %  values from datagen
+RB1 = str2double(vector(6)) ;     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+RB2 = str2double(vector(7)) ;
+VBEON = str2double(vector(8)) ;
+VCC = str2double(vector(9)) ;
+RS = str2double(vector(10)) ;
+RL = 8;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -45,70 +46,139 @@ RS = str2double(vector(10))
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-RB=1/(1/RB1+1/RB2)
-VEQ=RB2/(RB1+RB2)*VCC
-IB1=(VEQ-VBEON)/(RB+(1+BFN)*RE1)
-IC1=BFN*IB1
-IE1=(1+BFN)*IB1
-VE1=RE1*IE1
-VO1=VCC-RC1*IC1
-VCE=VO1-VE1
+RB = 1/(1/RB1+1/RB2);
+VEQ = RB2/(RB1+RB2)*VCC;
+
+% Colocar tudo isto 
+IB1 = (VEQ-VBEON)/(RB+(1+BFN)*RE1)
+IC1 = BFN*IB1
+IE1 = (1+BFN)*IB1
+VE1 = RE1*IE1
+VO1 = VCC-RC1*IC1
+VCE = VO1-VE1
+
+% isto também 
+gm1 = IC1/VT
+rpi1 = BFN/gm1
+ro1 = VAFN/IC1
 
 
-gm1=IC1/VT
-rpi1=BFN/gm1
-ro1=VAFN/IC1
 
-RSB=RB*RS/(RB+RS)
+RE1 = 100;
 
-AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-AVI_DB = 20*log10(abs(AV1))
-AV1simple = RB/(RB+RS) * gm1*RC1/(1+gm1*RE1)
-AVIsimple_DB = 20*log10(abs(AV1simple))
+% impedancia de entrada do transistor 1 : ZI1 = rpi || R2 || R1
 
-RE1=0
-AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-AVI_DB = 20*log10(abs(AV1))
-AV1simple =  - RSB/RS * gm1*RC1/(1+gm1*RE1)
-AVIsimple_DB = 20*log10(abs(AV1simple))
+ZI1 = RB1*RB2*rpi1/(RB1*RB2+ RB1*rpi1+ RB2*rpi1) 
 
-RE1=100
-ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
-ZX = ro1*((RSB+rpi1)*RE1/(RSB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RSB)+1/RE1+gm1*rpi1/(rpi1+RSB)))
-ZX = ro1*(   1/RE1+1/(rpi1+RSB)+1/ro1+gm1*rpi1/(rpi1+RSB)  )/(   1/RE1+1/(rpi1+RSB) ) 
-ZO1 = 1/(1/ZX+1/RC1)
+% Impedância de saída 1: Zo1 = r01||RC1
+ZO1 = ro1*RC1/(ro1+RC1)
 
-RE1=0
-ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
-ZO1 = 1/(1/ro1+1/RC1)
+% ganho de tensão 1 em módulo
+AV1 = gm1*ZO1*(ZI1/(ZI1+RS))
 
-%ouput stage
-BFP = 227.3
-VAFP = 37.2
-RE2 = 100
-VEBON = 0.7
+% ouput stage
+BFP = 227.3;
+VAFP = 37.2;
+RE2 = 100;
+VEBON = 0.7;
+
+% Colocar tudo isto 
 VI2 = VO1
 IE2 = (VCC-VEBON-VI2)/RE2
 IC2 = BFP/(BFP+1)*IE2
 VO2 = VCC - RE2*IE2
 
+% isto também 
 gm2 = IC2/VT
 go2 = IC2/VAFP
 gpi2 = gm2/BFP
 ge2 = 1/RE2
 
+% ganho de tensão 2 em módulo
 AV2 = gm2/(gm2+gpi2+go2+ge2)
+
+% impedancia de entrada do tansistor 2
+
 ZI2 = (gm2+gpi2+go2+ge2)/gpi2/(gpi2+go2+ge2)
+
+% Impedância de saída 2
 ZO2 = 1/(gm2+gpi2+go2+ge2)
 
+% Este rácio tem que ser aproximadamente 1
+% Confirmação da baixa perda de sinal entre os trasistores:
+b = ZI2/(ZI2+ ZO1)
 
-%total
-gB = 1/(1/gpi2+ZO1)
-AV = (gB+gm2/gpi2*gB)/(gB+ge2+go2+gm2/gpi2*gB)*AV1
-AV_DB = 20*log10(abs(AV))
-ZI=ZI1
-ZO=1/(go2+gm2/gpi2*gB+ge2+gB)
+% Confirmação que n há perda de sinal entre a fonte Vin e o primeiro transistor
+% deve ser aproximadamente 1
+a = ZI1/(ZI1+RS)
 
+% Confirmação que n há perda de sinal entre o egundo transistor e o altifalante
+% deve ser aproximadamente 1
+c = RL/(RL+ZO2)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% OP
+
+filename = "octave1.tex";
+fid = fopen (filename, "w+");
+
+fprintf(fid, "\\begin{table}[H] \n \\centering \n \\begin{tabular}{ c c } \n")
+fprintf(fid, "\\hline\n")
+fprintf(fid, "IB1 & %f \\\\ \n ", IB1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "IC1 & %f \\\\ \n ", IC1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "IE1 & %f \\\\ \n ", IE1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "VE1 & %f \\\\ \n ", VE1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "VO1 & %f \\\\ \n ", VO1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "VCE & %f \\\\ \n ", VCE)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "gm1 & %f \\\\ \n ", gm1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "rpi1 & %f \\\\ \n ", rpi1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ro1 & %f \\\\ \n ", ro1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ZI1 & %f \\\\ \n ", ZI1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ZO1 & %f \\\\ \n ", ZO1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "AV1 & %f \\\\ \n ", AV1)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "VI2 & %f \\\\ \n ", VI2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "IE2 & %f \\\\ \n ", IE2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "IC2 & %f \\\\ \n ", IC2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "VO2 & %f \\\\ \n ", VO2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "gm2 & %f \\\\ \n ", gm2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "go2 & %f \\\\ \n ", go2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "gpi2 & %f \\\\ \n ", gpi2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ge2 & %f \\\\ \n ", ge2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "AV2 & %f \\\\ \n ", AV2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ZI2 & %f \\\\ \n ", ZI2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "ZO2 & %f \\\\ \n ", ZO2)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "a & %f \\\\ \n ", a)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "b & %f \\\\ \n ", b)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "c & %f \\\\ \n ", c)
+fprintf(fid, "\\hline\n")
+fprintf(fid, "  \\end{tabular} \n \\caption{Operating point theoretically} \n \\label{tab:ex1} \n \\end{table} \n ")
+
+
+fclose (fid);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FIRST VALUES TABEL PRINT TO REPORT
 
@@ -117,12 +187,6 @@ fid = fopen (filename, "w+");
 
 fprintf(fid, "\\begin{table}[H] \n \\centering \n \\begin{tabular}{ c c } \n")
 fprintf(fid, "\\hline\n")
-fprintf(fid, "VT & %f \\\\ \n ", VT)
-fprintf(fid, "\\hline\n")
-fprintf(fid, "BFN & %f \\\\ \n ", BFN)
-fprintf(fid, "\\hline\n")
-fprintf(fid, "VAFN & %f \\\\ \n ", VAFN)
-fprintf(fid, "\\hline\n")
 fprintf(fid, "RE1 & %f \\\\ \n ", RE1)
 fprintf(fid, "\\hline\n")
 fprintf(fid, "RC1 & %f \\\\ \n ", RC1)
@@ -130,8 +194,6 @@ fprintf(fid, "\\hline\n")
 fprintf(fid, "RB1 & %f \\\\ \n ", RB1)
 fprintf(fid, "\\hline\n")
 fprintf(fid, "RB2 & %f \\\\ \n ", RB2)
-fprintf(fid, "\\hline\n")
-fprintf(fid, "VBEON & %f \\\\ \n ", VBEON)
 fprintf(fid, "\\hline\n")
 fprintf(fid, "VCC & %f \\\\ \n ", VCC)
 fprintf(fid, "\\hline\n")
@@ -147,14 +209,10 @@ fclose (fid);
 fid = fopen ('../sim/ngspice_data.txt', "w+");
 
 
-% fprintf(fid, ".param VT = %.11f\n", VT)
-% fprintf(fid, ".param BFN = %.11f\n", BFN)
-% fprintf(fid, ".param VAFN = %.11f\n", VAFN)
 fprintf(fid, ".param RE1 = %.11f\n", RE1)
 fprintf(fid, ".param RC1 = %.11f\n", RC1)
 fprintf(fid, ".param RB1 = %.11f\n", RB1)
 fprintf(fid, ".param RB2 = %.11f\n", RB2)
-% fprintf(fid, ".param VBEON = %.11f\n", VBEON)
 fprintf(fid, ".param VCC = %.11f\n", VCC)
 fprintf(fid, ".param RS = %.11f\n", RS)
 
